8a21da58e8407380645c3830df375baf1a5deae0
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-07-20 11:37:45 -0400

    Create filtering.sh

---
+++ filtering.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J filtering
+#SBATCH --mem=64000
+#SBATCH --output=filtering.out
+#SBATCH --error=filtering.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=END
+
+# USAGE: sbatch --mail-user=youremail@duke.edu filtering.sh /path/to/seq_data/current_project/dada2/ /path/to/metabarcoding.sif
+
+# load R via container 
+singularity exec --bind $1,$PWD $2 Rscript filtering.R filtering.Rout $1
