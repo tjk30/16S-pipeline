41703168ce5e4634b4265ea51e2e6ac2f5db49f8
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-07-20 12:20:30 -0400

    Update 6_dada2.sh

--- code/6_dada2.sh
+++ code/6_dada2.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 
-# USAGE: sbatch --mail-user=youremail@duke.edu dada2.sh /path/to/dada2 /path/to/metabarcoding.sif
+# USAGE: sbatch --mail-user=youremail@duke.edu dada2.sh /path/to/XXXXXXXX_results /path/to/metabarcoding.sif
 
 now=$(date +'%Y%m%d')
 singularity exec --bind $1,$PWD $2 Rscript dada2.R dada2.Rout $1 $now
