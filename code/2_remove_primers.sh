71f4b6110ae3a5337300ce0b77c0437457afe2bb
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-07-20 12:17:32 -0400

    Update 2_remove_primers.sh
    I am not sure if this is the correct command to call trimmomatic from inside the container

--- code/2_remove_primers.sh
+++ code/2_remove_primers.sh
@@ -7,16 +7,20 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 
-# USAGE: sbatch --mail-user=youremail@duke.edu remove_primers.sh /path/to/0_data_raw /path/to/metabarcoding.sif
-PATH1=$1
-
 # Removing primers see evernote note "Removing Primers from Illumina Data (Paired-End Fastq)
 # Using trimmomatic 
 # This step is recommended by dada2 in their FAQ on chimera checking
 # -trimlog trimLog.log 
 
+# USAGE: sbatch --mail-user=youremail@duke.edu remove_primers.sh /path/to/0_data_raw /path/to/metabarcoding.sif
+PATH1=$1
+cd $1
+cd ..
+mkdir 1_remove_primers
+mkdir 1_remove_primers/run1
+
 # For Sequencing Run 1
-singularity exec --bind $1 $2 java -jar trimmomatic-0.36.jar PE \
+singularity exec --bind $PWD $2 java -jar trimmomatic-0.36.jar PE \
 $PATH1/Undetermined_S0_L001_R1_001.fastq.gz \
 $PATH1/Undetermined_S0_L001_R2_001.fastq.gz \
 ./1_remove_primers/run1/R1.paired.fastq.gz \
