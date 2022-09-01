3cc4671b0cb59d4bee8f1aad4966aac4e216dbcc
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-09-01 15:05:18 -0400

    Update 4_demultiplex.sh
    pull qiime2 from 16S-analysis container

--- code/4_demultiplex.sh
+++ code/4_demultiplex.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 
-# USAGE: sbatch --mail-user=youremail@duke.edu 4_demultiplex.sh /path/to/XXXXXXXX_results /path/to/qiime2-env
+# USAGE: sbatch --mail-user=youremail@duke.edu 4_demultiplex.sh /path/to/XXXXXXXX_results /path/to/16S-analysis.sif
 cd $1
 mkdir 3_demultiplex
 mkdir 3_demultiplex/demuxd_reads
@@ -18,20 +18,18 @@ OUTPUT2=3_demultiplex/demuxd_reads
 MAP1=0_mapping/barcodes.tsv
 
 # Qiime module failing due to missing R/3.1.0-fasrc01 dependency. Adjusting with qiime2
-# Following https://docs.qiime2.org/2021.8/tutorials/atacama-soils/#atacama-demux
+# Pulling from qiime2/core docker image
 
 # Load qiime2
-module load Anaconda3/5.1.0
-conda activate $2
 
 # Convert our three files (index, read1, read2) into an EMPPairedEndSequences object
-qiime tools import \
+singularity exec --bind $1 $3 qiime tools import \
    --type EMPPairedEndSequences \
    --input-path $INPUT \
    --output-path $OUTPUT/emp-paired-end-sequences.qza
 
 # Demultiplex sequence reads
-qiime demux emp-paired \
+singularity exec --bind $1 $3 qiime demux emp-paired \
   --m-barcodes-file $MAP1 \
   --m-barcodes-column barcode-sequence \
   --p-rev-comp-mapping-barcodes \
@@ -40,6 +38,6 @@ qiime demux emp-paired \
   --o-error-correction-details $OUTPUT/demux-details.qza
 
 # Convert .qza to .fastq
-qiime tools export \
+singularity exec --bind $1 $3 qiime tools export \
   --input-path $OUTPUT/demux-full.qza \
   --output-path $OUTPUT2
