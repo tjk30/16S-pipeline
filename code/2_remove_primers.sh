4213f92407819acbcb228ca38aa4f6276e324317
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-09-28 14:57:04 -0400

    Update 2_remove_primers.sh

--- code/2_remove_primers.sh
+++ code/2_remove_primers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J remove_primers
+#SBATCH -J 2_remove_primers
 #SBATCH --partition scavenger
 #SBATCH --mem=16000
-#SBATCH --output=remove_primers.out
-#SBATCH --error=remove_primers.err
+#SBATCH --output=2_remove_primers-%J.out
+#SBATCH --error=2_remove_primers-%J.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 
@@ -12,7 +12,7 @@
 # This step is recommended by dada2 in their FAQ on chimera checking
 # -trimlog trimLog.log 
 
-# USAGE: sbatch --mail-user=youremail@duke.edu remove_primers.sh /path/to/0_data_raw /path/to/metabarcoding.sif
+# USAGE: sbatch --mail-user=youremail@duke.edu 2_remove_primers.sh /path/to/0_data_raw /path/to/16s-analysis.sif /path/to/techseqs.fa
 PATH1=$1
 cd $1
 cd ..
@@ -20,11 +20,11 @@ mkdir 1_remove_primers
 mkdir 1_remove_primers/run1
 
 # For Sequencing Run 1
-singularity exec --bind $PWD $2 java -jar trimmomatic-0.36.jar PE \
+singularity exec --bind $PWD,$3 $2 trimmomatic PE -phred64  \
 $PATH1/Undetermined_S0_L001_R1_001.fastq.gz \
 $PATH1/Undetermined_S0_L001_R2_001.fastq.gz \
 ./1_remove_primers/run1/R1.paired.fastq.gz \
 ./1_remove_primers/run1/R1.unpaired.fastq.gz \
 ./1_remove_primers/run1/R2.paired.fastq.gz \
 ./1_remove_primers/run1/R2.unpaired.fastq.gz \
-ILLUMINACLIP:techseqs.fa:2:30:10 
+ILLUMINACLIP:$3:2:30:10 
