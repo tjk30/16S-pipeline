ecaf601d946d47ad036f82697f72c7f6da747f92
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-09-28 14:55:51 -0400

    Update 5_filtering.sh

--- code/5_filtering.sh
+++ code/5_filtering.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -J 5_filtering
 #SBATCH --mem=64000
-#SBATCH --partition scavenger
-#SBATCH --output=filtering.out
-#SBATCH --error=filtering.err
+#SBATCH --output=filtering-%J.out
+#SBATCH --error=filtering-%J.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
+#SBATCH -p scavenger 
 
 # USAGE: sbatch --mail-user=youremail@duke.edu 5_filtering.sh /path/to/seq_data/current_project/dada2/ /path/to/metabarcoding.sif
 
 # load R via container 
-singularity exec --bind $1,$PWD $2 Rscript filtering.R filtering.Rout $1
+singularity exec --bind $1,$PWD $2 Rscript 5_filtering.R $1
