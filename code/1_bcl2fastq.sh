04f1d65f9cb94790d72a7238efe01bc6c43621be
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-09-28 14:56:28 -0400

    Update 1_bcl2fastq.sh

--- code/1_bcl2fastq.sh
+++ code/1_bcl2fastq.sh
@@ -2,17 +2,22 @@
 #SBATCH -J bcl2fastq
 #SBATCH --partition scavenger
 #SBATCH --mem=64000
-#SBATCH --output=bcl2fastq-%j.out
-#SBATCH --error=bcl2fastq-%j.err
+#SBATCH --output=bcl2fastq-%J.out
+#SBATCH --error=bcl2fastq-%J.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 
-# USAGE: sbatch --mail-user=<youremail>@duke.edu 1_bcl2fastq.sh /path/to/miniseqDir /path/to/metabarcoding.sif SampleSheet.csv
+# USAGE: sbatch 1_bcl2fastq.sh /path/to/miniseqDir /path/to/metabarcoding.sif SampleSheet.csv
 cd $1
 cd ..
+parent=$PWD
 now=$(date +'%Y%m%d')
 outdir=$now'_results'
 mkdir $outdir
 mkdir $outdir/0_data_raw
+mkdir $outdir/0_data_test
 
-singularity exec --bind $PWD $2 bcl2fastq -R $1 -o ./0_data_raw --create-fastq-for-index-reads --sample-sheet $3
+singularity exec --bind $parent $2 bcl2fastq -R $1 -o ./0_data_raw --create-fastq-for-index-reads --sample-sheet $3
+
+singularity exec --bind $parent $2 bcl2fastq -R . -o ./0_data_test
+
