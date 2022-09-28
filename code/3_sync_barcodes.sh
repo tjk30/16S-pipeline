98e07619ebf096b86f41a0cf54ef46058ce45513
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-09-28 14:54:39 -0400

    Update 3_sync_barcodes.sh

--- code/3_sync_barcodes.sh
+++ code/3_sync_barcodes.sh
@@ -2,30 +2,26 @@
 #SBATCH -J sync_barcodes
 #SBATCH --partition scavenger
 #SBATCH --mem=16000
-#SBATCH --output=sync_barcodes.out
-#SBATCH --error=sync_barcodes.err
+#SBATCH --output=3_sync_barcodes-%J.out
+#SBATCH --error=3_sync_barcodes-%J.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
+#SBATCH -p scavenger 
 
-# USAGE: sbatch --mail-user=<youremail>@duke.edu 3_sync_barcodes.sh /path/to/XXXXXXXX_results 
+# USAGE: sbatch --mail-user=<youremail>@duke.edu 3_sync_barcodes.sh /path/to/XXXXXXXX_results /path/to/16s-analysis.sif
 
 PATH1=$1
+cd $PATH1
 mkdir 2_sync_barcodes
-mkdir 2_sync_barcodes/run1
+mkdir 2_sync_barcodes/for-qiime2
 
-#Since we just filtered the Forward and Reverse reads, this step 
-#ensures the index files also drop any reads removed in the prior step. 
+BARCODE1=$PATH1/0_data_raw/Undetermined_S0_L001_I1_001.fastq.gz
 
-BARCODE1=0_data_raw/Undetermined_S0_L001_I1_001.fastq.gz
+singularity exec --bind $1 $2 repair.sh in1=$BARCODE1 in2=$PATH1/1_remove_primers/R1.paired.fastq.gz out1=$PATH1/2_sync_barcodes/I1.synced.fastq.gz out2=$PATH1/2_sync_barcodes/R1.paired.fastq.gz outs=$PATH1/2_sync_barcodes/singletons.fq repair
 
-cp $PATH1/1_remove_primers/run1/R2.paired.fastq.gz $PATH1/2_sync_barcodes/run1/R2.paired.fastq.gz
-
-$PATH1/sync_paired_end_reads.py $BARCODE1 $BARCODE1 \
-$PATH1/1_remove_primers/run1/R1.paired.fastq.gz \
-$PATH1/2_sync_barcodes/run1/I1.synced.fastq.gz \
-$PATH1/2_sync_barcodes/run1/R1.paired.fastq.gz
+singularity exec --bind $1 $2 repair.sh in1=$BARCODE1 in2=$PATH1/1_remove_primers/R2.paired.fastq.gz out1=$PATH1/2_sync_barcodes/I1.synced.R2.fastq.gz out2=$PATH1/2_sync_barcodes/R2.paired.fastq.gz outs=$PATH1/2_sync_barcodes/singletons.R2.fq repair
 
 # Rename output files to be accepted by qiime2 demultiplex step
-mv $PATH1/1_remove_primers/run1/R1.paired.fastq.gz $PATH1/1_remove_primers/run1/forward.fastq.gz
-mv $PATH1/1_remove_primers/run1/R2.paired.fastq.gz $PATH1/1_remove_primers/run1/reverse.fastq.gz
-mv $PATH1/1_remove_primers/run1/I1.synced.fastq.gz $PATH1/1_remove_primers/run1/barcodes.fastq.gz
+mv $PATH1/2_repair/R1.paired.fastq.gz $PATH1/2_sync_barcodes/for-qiime2/forward.fastq.gz
+mv $PATH1/2_repair/R2.paired.fastq.gz $PATH1/2_sync_barcodes/for-qiime2/reverse.fastq.gz
+mv $PATH1/2_repair/I1.synced.fastq.gz $PATH1/2_sync_barcodes/for-qiime2/barcodes.fastq.gz
