55814dc52515896b80c68495a0d241befcc93d04
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-09-29 15:59:57 -0400

    Update 6_dada2.sh

--- code/6_dada2.sh
+++ code/6_dada2.sh
@@ -8,7 +8,8 @@
 #SBATCH -p scavenger 
 
 # USAGE: sbatch --mail-user=youremail@duke.edu 6_dada2.sh /path/to/XXXXXXXX_results /path/to/mapping.txt /path/to/silva-database-dir /path/to/16s-analysis.sif 
+wd=$PWD
 cd $1
 mkdir 5_dada2
-singularity exec --bind $1,$PWD,$2,$3 $4 Rscript 6_dada2.R $1 $2 $3
+singularity exec --bind $1,$wd,$2,$3 $4 Rscript 6_dada2.R $1 $2 $3
 
