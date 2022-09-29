8731161c8b6574e0e14c4cd977c9ac32ee4a98f4
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-09-29 15:51:35 -0400

    Update 5_filtering.sh

--- code/5_filtering.sh
+++ code/5_filtering.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -J 5_filtering
 #SBATCH --mem=64000
-#SBATCH --output=filtering-%J.out
-#SBATCH --error=filtering-%J.err
+#SBATCH --output=5_filtering-%J.out
+#SBATCH --error=5_filtering-%J.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 #SBATCH -p scavenger 
 
-# USAGE: sbatch --mail-user=youremail@duke.edu 5_filtering.sh /path/to/seq_data/current_project/dada2/ /path/to/metabarcoding.sif
+# USAGE: sbatch --mail-user=youremail@duke.edu 5_filtering.sh /path/to/XXXXXXXX_results /path/to/16s-analysis.sif
 
 # load R via container 
 singularity exec --bind $1,$PWD $2 Rscript 5_filtering.R $1
