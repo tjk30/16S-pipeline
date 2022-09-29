4990d5d38a79da5ecc69e179f4e32dbe8c279313
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-09-29 15:59:28 -0400

    Update 6_dada2.sh

--- code/6_dada2.sh
+++ code/6_dada2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -J dada2
+#SBATCH -J 6_dada2
 #SBATCH --mem=128000
-#SBATCH --output=5_dada2.out
-#SBATCH --error=5_dada2.err
+#SBATCH --output=6_dada2-%J.out
+#SBATCH --error=6_dada2-%J.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
+#SBATCH -p scavenger 
 
-# USAGE: sbatch --mail-user=youremail@duke.edu dada2.sh /path/to/XXXXXXXX_results /path/to/metabarcoding.sif /path/to/silva-database
-# silva-database is the same as this folder on Box: /project_davidlab/LAD_LAB_Personnel/Jeff/HARDAC/16S_pipeline_JL21/0_training
-
-now=$(date +'%Y%m%d')
-singularity exec --bind $1,$PWD $2 Rscript dada2.R dada2.Rout $1 $3 $now 
+# USAGE: sbatch --mail-user=youremail@duke.edu 6_dada2.sh /path/to/XXXXXXXX_results /path/to/mapping.txt /path/to/silva-database-dir /path/to/16s-analysis.sif 
+cd $1
+mkdir 5_dada2
+singularity exec --bind $1,$PWD,$2,$3 $4 Rscript 6_dada2.R $1 $2 $3
 
