8b4d74f805d1f72d1bb7b47c02f6d887ddf61459
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-11-08 10:44:39 -0500

    move .out and .err files after completion

--- code/1_bcl2fastq.sh
+++ code/1_bcl2fastq.sh
@@ -2,12 +2,13 @@
 #SBATCH -J bcl2fastq
 #SBATCH --partition scavenger
 #SBATCH --mem=64000
-#SBATCH --output=bcl2fastq-%J.out
-#SBATCH --error=bcl2fastq-%J.err
+#SBATCH --output=1_bcl2fastq.out
+#SBATCH --error=1_bcl2fastq.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 
 # USAGE: sbatch 1_bcl2fastq.sh /path/to/miniseqDir /path/to/metabarcoding.sif SampleSheet.csv
+codedir=$PWD
 cd $1
 cd ..
 parent=$PWD
@@ -15,6 +16,11 @@ now=$(date +'%Y%m%d')
 outdir=$now'_results'
 mkdir $outdir
 mkdir $outdir/0_data_raw
+mkdir $outdir/Reports
 
 singularity exec --bind $parent $2 bcl2fastq -R $1 -o $outdir/0_data_raw --create-fastq-for-index-reads --sample-sheet $3
+
+# move .err and .out files
+mv $codedir/1_bcl2fastq.out $outdir/Reports
+mv $codedir/1_bcl2fastq.err $outdir/Reports
 
