f54b3f98c5649ec830fe6ed17517cfac0fe772f4
tjk30 <83459346+tjk30@users.noreply.github.com>
2022-11-08 10:50:43 -0500

    Update 5_filtering.sh

--- code/5_filtering.sh
+++ code/5_filtering.sh
@@ -8,10 +8,9 @@
 #SBATCH -p scavenger 
 
 # USAGE: sbatch --mail-user=youremail@duke.edu 5_filtering.sh /path/to/XXXXXXXX_results /path/to/16s-analysis.sif
-codedir=$PWD
 # load R via container 
 singularity exec --bind $1,$PWD $2 Rscript 5_filtering.R $1
 
 # move .err and .out files
-mv $codedir/5_filtering.out $1/Reports
-mv $codedir/5_filtering.err $1/Reports
+mv $PWD/5_filtering.out $1/Reports
+mv $PWD/5_filtering.err $1/Reports
