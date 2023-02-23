5a16cd9251e5f99f87852a384427145e97158635
tjk30 <83459346+tjk30@users.noreply.github.com>
2023-02-23 12:04:35 -0500

    Update 5_dada2.sh

--- code/5_dada2.sh
+++ code/5_dada2.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-#SBATCH -J 5_dada2
+#SBATCH -J dada2
 #SBATCH --mem=128000
-#SBATCH --output=5_dada2.out
-#SBATCH --error=5_dada2.err
+#SBATCH --output=6_dada2-%J.out
+#SBATCH --error=6_dada2-%J.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 #SBATCH -p scavenger 
 
-# USAGE: sbatch --mail-user=youremail@duke.edu 5_dada2.sh /path/to/XXXXXXXX_results /path/to/mapping.txt /path/to/silva-database-dir /path/to/metabarcoding.sif 
-mkdir $1/4_dada2
-singularity exec --bind $1,$PWD,$2,$3 $4 Rscript 5_dada2.R $1 $2 $3
+# USAGE: sbatch --mail-user=youremail@duke.edu 5_dada2.sh /path/to/XXXXXXXX_results /path/to/silva-database-dir /path/to/metabarcoding.sif 
 
-# move .err and .out files
-mv $PWD/5_dada2.out $1/Reports
-mv $PWD/5_dada2.err $1/Reports
+singularity exec --bind $1,$PWD,$2 $3 Rscript 5_dada2.R $1 $2 
